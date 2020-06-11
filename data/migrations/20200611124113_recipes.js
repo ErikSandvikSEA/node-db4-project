659579945f616874cc5c72423943b04a00a50f32
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-06-11 15:30:14 -0700

    knexfile and schema up and running

--- data/migrations/20200611124113_recipes.js
+++ data/migrations/20200611124113_recipes.js
@@ -1,8 +1,56 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+     .createTable('ingredients', tbl => {
+          tbl.increments();
+          tbl.string('ingredient_name').notNullable().index()
+          tbl.integer('ingredient_quantity').notNullable();
+     })
+     .createTable('steps', tbl => {
+          tbl.increments();
+          tbl.string('step_text').notNullable().index();
+     })
+     .createTable('recipes', tbl => {
+          tbl.increments()
+          tbl.string('recipe_name').notNullable().index();
+     })
+     .createTable('instructions', tbl => {
+          tbl.increments();
+          tbl
+               .integer("recipe_id")
+               .unsigned()
+               .references('recipes.id')
+               .onDelete('RESTRICT')
+               .onUpdate('CASCADE');
+          tbl
+               .integer('step_number')
+               .unsigned()
+               .references('steps.id')
+               .onDelete('RESTRICT')
+               .onUpdate('CASCADE');
+     })
+     .createTable('recipe_shopping_list', tbl => {
+          tbl.increments()
+          tbl
+               .integer("recipe_id")
+               .unsigned()
+               .references('recipes.id')
+               .onDelete('RESTRICT')
+               .onUpdate('CASCADE');
+          tbl
+               .integer('ingredient_id')
+               .unsigned()
+               .references('ingredients.id')
+               .onDelete('RESTRICT')
+               .onUpdate('CASCADE');
+     })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+  .dropTableIfExists('recipe_shopping_list')
+  .dropTableIfExists('instructions')
+  .dropTableIfExists('recipes')
+  .dropTableIfExists('ingredients')
+  .dropTableIfExists('steps')
 };
